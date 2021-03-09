4a989e7958d16db2c6871e0207d444e3073c0292
anhnguyet2305 <sonbnnth2007009@fpt.edu.vn>
2021-03-09 08:00:12 +0700

    ..

---
+++ src/LabS8/KhachHang.java
@@ -0,0 +1,44 @@
+package LabS8;
+
+import java.util.Scanner;
+
+public abstract class KhachHang {
+    protected int maKH;
+    protected String tenKh;
+    protected String ngayXuatHD;
+    protected int soLuong;
+
+    public int getMaKH() {
+        return maKH;
+    }
+
+    public void setMaKH(int maKH) {
+        this.maKH = maKH;
+    }
+
+    public String getTenKh() {
+        return tenKh;
+    }
+
+    public void setTenKh(String tenKh) {
+        this.tenKh = tenKh;
+    }
+
+    public String getNgayXuatHD() {
+        return ngayXuatHD;
+    }
+
+    public void setNgayXuatHD(String ngayXuatHD) {
+        this.ngayXuatHD = ngayXuatHD;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public abstract double thanhTien();
+}
---
+++ src/LabS8/KhachHangNuocNgoai.java
@@ -0,0 +1,19 @@
+package LabS8;
+
+public class KhachHangNuocNgoai extends KhachHang {
+    public String quocTich;
+
+    public String getQuocTich() {
+        return quocTich;
+    }
+
+    public void setQuocTich(String quocTich) {
+        this.quocTich = quocTich;
+    }
+
+    @Override
+    public double thanhTien() {
+        return getSoLuong() * 2000;
+    }
+}
+
