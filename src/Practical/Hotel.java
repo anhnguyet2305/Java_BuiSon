f3a59cc1968dad2bcfe61bc0118a1a7ae8adc837
anhnguyet2305 <sonbnnth2007009@fpt.edu.vn>
2021-03-12 10:24:12 +0700

    ..

---
+++ src/Practical/Hotel.java
@@ -0,0 +1,47 @@
+package Practical;
+
+import java.util.Scanner;
+
+public class Hotel {
+    String name;
+    String location;
+    String ownerName;
+    String Location;
+
+    public Hotel() {
+    }
+
+    public Hotel(String name, String location, String ownerName, String location1) {
+        this.name = name;
+        this.location = location;
+        this.ownerName = ownerName;
+        Location = location1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+
+
+}
---
+++ src/Practical/Main.java
@@ -0,0 +1,49 @@
+package Practical;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static Hotel[] hotels;
+
+    public static void main(String[] args){
+        hotels = new Hotel[10];
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your hotel information!");
+        for (int i = 0; i <10; ++i){
+            System.out.println("Hotel : " +i );
+
+            System.out.println("name : ");
+            String name = scanner.next();
+
+            System.out.println("location : ");
+            String location = scanner.next();
+
+            System.out.println("ownerName : ");
+            String ownerName = scanner.next();
+
+            System.out.println("Location : ");
+            String Location = scanner.next();
+
+            hotels[i] = new Hotel(name, location, ownerName, Location);
+        }
+
+
+
+        System.out.println("Please enter the owner name of the hotel!");
+        String ownerName = scanner.next();
+        searchByOwnerName(ownerName);
+
+    }
+
+    public static void searchByOwnerName(String ownerName){
+        System.out.println("All hotels of " + ownerName + ":");
+        for (Hotel hotel: hotels) {
+            if (hotel.ownerName.equals(ownerName)) {
+                System.out.println(hotel.toString());
+            }
+        }
+    }
+}
